669333db00fa83518a3dd64ce556b488350b2d2a
Calvin Wylie <calvin.wylie@gmail.com>
2015-11-24 09:40:04 -0500

    Added fixed return asset

--- src/portfolio.c
+++ src/portfolio.c
@@ -6,14 +6,16 @@
 
 int main(int argc, char** argv)
 {
-    const int n = 10;
-    const int N = 10;
-    const int size = (N+1)*(n+1);
+    const int n = 200;
+    const int N = 5312;
+    const int size = (N+1)*(n+1) + 1; // We add one because GLPK indexes arrays
+                                      // starting at 1 instead of 0.
   
     /* declare variables */
     glp_prob *lp;
-    int ia[size], ja[size];
-    double ar[size];
+    int* ia = malloc(size * sizeof(int));
+    int* ja = malloc(size * sizeof(int));
+    double* ar = malloc(size * sizeof(double));
   
     /* create problem */
     lp = glp_create_prob();
@@ -49,14 +51,21 @@ int main(int argc, char** argv)
     int idx = 1;
     // Random constraints
     for (int i = 1; i <= N; i++) {
-        for (int j = 1; j <= n; j++) {
+        for (int j = 1; j < n; j++) {
             ia[idx] = i;
             ja[idx] = j;
-            double r = genrand(&state) + 1;
+            double r = 0.1*genrand(&state) + 0.01; // [0.0, 0.1]
             // printf("r=%f\n", r);
-            ar[idx] = r;
+            ar[idx] = 1.0+r;
             idx += 1;
         }
+
+        // Fixed return asset
+        ia[idx] = i;
+        ja[idx] = n;
+        ar[idx] = 1.05;
+        idx += 1;
+
         // t
         ia[idx] = i;
         ja[idx] = n+1;
@@ -78,7 +87,7 @@ int main(int argc, char** argv)
     ar[idx] = 0.0;
     idx += 1;
 
-    glp_load_matrix(lp, size, ia, ja, ar);
+    glp_load_matrix(lp, size-1, ia, ja, ar);
   
     /* solve problem */
     glp_simplex(lp, NULL);
@@ -88,17 +97,19 @@ int main(int argc, char** argv)
     for (int i = 1; i <= n; i++) {
         printf("y%d = %g\n", i, glp_get_col_prim(lp, i));
     }
-    printf("t = %g\n", glp_get_col_prim(lp, n+1));
+    // printf("t = %g\n", glp_get_col_prim(lp, n+1));
   
     /* recover dual variables at optimality
-       when y > 0, the constraint is active */
-    // y1 = glp_get_row_dual(lp, 1);
-    // y2 = glp_get_row_dual(lp, 2);
-    // y3 = glp_get_row_dual(lp, 3);
-    // printf("y1 = %g; y2 = %g; y3 = %g\n", y1, y2, y3);
+       when dual < 0, the constraint is active */
+    // for (int i = 1; i <= N; i++) {
+    //     printf("constr%d = %g\n", i, glp_get_row_dual(lp, i));
+    // }
   
     /* housekeeping */
     glp_delete_prob(lp);
     glp_free_env();
+    free(ia);
+    free(ja);
+    free(ar);
     return 0;
 }
